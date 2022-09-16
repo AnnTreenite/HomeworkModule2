d073dd6508521e1bb3416cdfa9c8d653edd64264
Ann <ann.treenite@gmail.com>
2022-09-16 19:53:54 +0300

    ДЗ 4 закончила как поняла

--- src/main/java/Homework4/Runner.java
+++ src/main/java/Homework4/Runner.java
@@ -1,7 +1,34 @@
 package main.java.Homework4;
 
-public class Runner {
-    public static void main(String[] args) {
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+public class Runner {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        UserMeta human = new UserMeta();
+
+        //3.3 При нахождении @GetMetaData над методом, печатаем сколько времени выполнялся метод.
+        for (Method declaredMethod : UserMeta.class.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(GetMetaData.class)) {
+                long start = System.currentTimeMillis();
+                declaredMethod.invoke(human);
+                long end = System.currentTimeMillis();
+                System.out.println(end - start);
+            }
+        }
+        //3.2 При нахождении @GetMetaData над атрибутом, чтобы печаталось значение атрибута этого поля
+        for (Field declaredField : human.getClass().getDeclaredFields()) {
+            if (declaredField.isAnnotationPresent(GetMetaData.class)) {
+                declaredField.setAccessible(true);
+                System.out.println(declaredField);
+            }
+        }
+        //3.1 При нахождении @GetMetaData над классом, то печатаем все атрибуты класса
+        for (Method declaredMethod : UserMeta.class.getDeclaredMethods()) {
+            if (declaredMethod.isAnnotationPresent(GetMetaData.class)) {
+                System.out.println(declaredMethod);
+            }
+        }
     }
 }
